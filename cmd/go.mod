d6e3878a2cc7191f574d434af895eb3cec4fe5da
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-11-12 09:10:48 +0000

    Update tester command
    Update Ubuntu image for example since DigitalOcean removed
    the image being used.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- cmd/go.mod
+++ cmd/go.mod
@@ -2,6 +2,6 @@ module github.com/inlets/cloud-provision/cmd
 
 go 1.16
 
-replace github.com/inlets/cloud-provision => ../
+replace github.com/inlets/cloud-provision/provision => ../
 
-require github.com/inlets/cloud-provision/provision v0.0.0-20210717102404-016f5231b09e
+require github.com/inlets/cloud-provision v0.5.5
--- cmd/main.go
+++ cmd/main.go
@@ -11,14 +11,19 @@ import (
 )
 
 func main() {
-	var accessToken string
-	var userDataFile string
-	var userdata string
-	var hostname string
+	var (
+		accessToken  string
+		userDataFile string
+		userdata     string
+		hostname     string
+		region       string
+	)
 
 	flag.StringVar(&accessToken, "access-token", "", "Access token for provisioning a host")
 	flag.StringVar(&userDataFile, "userdata-file", "", "Apply user-data from a file to configure the host")
 	flag.StringVar(&hostname, "hostname", "provision-example", "Name for the host")
+	flag.StringVar(&region, "region", "lon1", "Region for the host")
+
 	flag.Parse()
 
 	if len(accessToken) == 0 {
@@ -28,14 +33,14 @@ func main() {
 
 	provisioner, err := provision.NewDigitalOceanProvisioner(accessToken)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, err.Error())
+		fmt.Fprintf(os.Stderr, "%s", err.Error())
 		os.Exit(1)
 	}
 
 	if len(userDataFile) > 0 {
 		res, err := ioutil.ReadFile(userDataFile)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
+			fmt.Fprintf(os.Stderr, "%s", err.Error())
 			os.Exit(1)
 		}
 
@@ -45,15 +50,15 @@ func main() {
 	// Find examples here for other clouds -> https://github.com/inlets/inletsctl/blob/356886f41e7c48a9644a24532027d1defa1d69e8/cmd/create.go
 	res, err := provisioner.Provision(provision.BasicHost{
 		Name:       hostname,
-		OS:         "ubuntu-16-04-x64",
-		Plan:       "512mb",
-		Region:     "lon1",
+		OS:         "ubuntu-18-04-x64",
+		Plan:       "s-1vcpu-1gb",
+		Region:     region,
 		UserData:   userdata,
 		Additional: map[string]string{},
 	})
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, err.Error())
+		fmt.Fprintf(os.Stderr, "%s", err.Error())
 		os.Exit(1)
 	}
 
@@ -66,7 +71,7 @@ func main() {
 		res, err := provisioner.Status(res.ID)
 
 		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
+			fmt.Fprintf(os.Stderr, "%s", err.Error())
 			os.Exit(1)
 		}
 		if res.Status == provision.ActiveStatus {
@@ -75,5 +80,4 @@ func main() {
 		}
 		time.Sleep(waitInterval)
 	}
-
 }
