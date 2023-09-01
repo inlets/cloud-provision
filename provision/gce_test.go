135390c302ece0dd575c721078292630019f4922
Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>
2023-09-01 16:12:28 +0200

    Fix gce tests
    Signed-off-by: Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>

--- provision/gce_test.go
+++ provision/gce_test.go
@@ -6,16 +6,18 @@ func TestCustomGCEIDConstAndDest(t *testing.T) {
 	inputInstanceName := "inlets"
 	inputZone := "us-central1-a"
 	inputProjectID := "playground"
+	inputRegion := "us-central"
 
-	customID := toGCEID(inputInstanceName, inputZone, inputProjectID)
+	customID := toGCEID(inputInstanceName, inputZone, inputProjectID, inputRegion)
 
-	outputInstanceName, outputZone, outputProjectID, err := getGCEFieldsFromID(customID)
+	outputInstanceName, outputZone, outputProjectID, outputRegion, err := getGCEFieldsFromID(customID)
 	if err != nil {
 		t.Error(err)
 	}
 	if inputInstanceName != outputInstanceName ||
 		inputZone != outputZone ||
-		inputProjectID != outputProjectID {
+		inputProjectID != outputProjectID ||
+		inputRegion != outputRegion {
 		t.Errorf("Input fields: %s, %s, %s are not equal to the ouput fields: %s, %s, %s",
 			inputInstanceName, inputZone, inputProjectID, outputInstanceName, outputZone, outputProjectID)
 	}
